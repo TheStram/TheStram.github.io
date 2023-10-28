0a7aed04598075b1476ef724b74e2485a19a2151
TheStram <mar.vinlopz3004@gmail.com>
2023-10-27 22:15:26 -0600

    logo switch add

--- page/assets/scripts/sty_switch.js
+++ page/assets/scripts/sty_switch.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Obtiene el elemento del interruptor por su ID
     const styleSwitch = document.getElementById("style-switch");
+    const logoImage = document.getElementById("Logo00");
   
     // Verifica el estado del interruptor y establece la hoja de estilo correspondiente
     styleSwitch.addEventListener("change", function () {
       if (styleSwitch.checked) {
         // Si el interruptor está activado (dark mode)
         document.getElementById("style-link").setAttribute("href", "assets/styles/styles-dark.css");
+        logoImage.setAttribute("src", "assets/images/Logo-night.png");
       } 
       else {
         // Si el interruptor está desactivado (light mode)
         document.getElementById("style-link").setAttribute("href", "assets/styles/styles-light.css");
+        logoImage.setAttribute("src", "assets/images/Logo-day.png");
       }
     });
   });
